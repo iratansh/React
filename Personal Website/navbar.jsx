f9b00a0719fed487bb351475dd04c8efa6cef8b2
Ishaan Ratanshi <151393106+iratansh@users.noreply.github.com>
2024-05-14 08:27:30 -0600

    Update navbar.jsx

--- Personal Website/navbar.jsx
+++ Personal Website/navbar.jsx
@@ -1,23 +1,54 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Icon } from "react-icons-kit";
+import { home } from "react-icons-kit/icomoon/home";
+import { ic_person } from "react-icons-kit/md/ic_person";
+import { MdDescription } from "react-icons/md";
+import { FaEnvelope } from "react-icons/fa"; 
 
 export default function NavigationBar() {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Navbar
+        fixed="bottom"
+        style={{
+          backgroundColor: "gray",
+          opacity: 0.75,
+          maxHeight: "70px",
+          maxWidth: "300px",
+          borderRadius: "20px",
+          margin: "auto",
+          marginBottom: "30px",
+        }}
+      >
+        <Container
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            verticalAlign: "center",
+            marginBottom: "40px",
+          }}
+        >
+          <Nav className="navbar">
+            <Nav.Link href="#home">
+              <Icon icon={home} size={24} />
+            </Nav.Link>
+            <Nav.Link href="#about">
+              <Icon icon={ic_person} size={24} />
+            </Nav.Link>
+            <Nav.Link href="#portfolio">
+              <MdDescription size={24} />
+            </Nav.Link>{" "}
+            <Nav.Link href="#contact">
+              <FaEnvelope size={24} />
+            </Nav.Link>{" "}
           </Nav>
         </Container>
       </Navbar>
-
     </>
   );
 }
